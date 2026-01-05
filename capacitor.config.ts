0d95add0a3c491ccdc312e904e442979c0123b51
Vite Asus ROG <luigifvite@gmail.com>
2026-01-04 23:11:13 -0500

    cambios finales

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,18 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'energia-app',
-  webDir: 'www'
+appId: 'com.energiafm.app',
+  appName: 'Energía FM',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: '#0097B2',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false
+    }
+  }
 };
 
 export default config;
